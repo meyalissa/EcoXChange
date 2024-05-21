5888d4470e6d08dfe71429d3ec49d0a7311bb55f
Melissa Sofia <melissa.sofia0204@gmail.com>
2024-05-21 18:29:40 +0800

    Update calculator-2.php

--- EcoXchange/pages/calculator-2.php
+++ EcoXchange/pages/calculator-2.php
@@ -39,85 +39,10 @@ function calculateSubtotal($itemName, $weight) {
 <body>
     <!-- =============== Navigation ================ -->
     <div class="container">
-        <div class="navigation">
-            <ul>
-                <li class = "comp-name">
-                    <a href="#">
-                        <span class="icon">
-                            <img src="../images/logo-white-border.png" class="sidebar-logo" />
-                        </span>
-                        <span class="title">EcoXchange</span>
-                    </a>
-                </li>
-             
-
-                <li class="active" >
-                    <a href="dashboard-2.php">
-                        <span class="icon">
-                            <i class='bx bx-history'></i>
-                        </span>
-                        <span class="title">Dashboard</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="items-2.html">
-                        <span class="icon">
-                            <i class='bx bxl-spring-boot'></i>
-                        </span>
-                        <span class="title">Items</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="calculator-2.html">
-                        <span class="icon">
-                            <i class='bx bxs-calculator'></i>
-                        </span>
-                        <span class="title">Calculator</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="profile-2.html">
-                        <span class="icon">
-                            <i class='bx bx-user-circle'></i>
-                        </span>
-                        <span class="title">Profile</span>
-                    </a>
-                </li>
-
-                <li class="sign-out">
-                    <a href="../includes/logout.inc.php">
-                        <span class="icon">
-                            <ion-icon name="log-out-outline"></ion-icon>
-                        </span>
-                        <span class="title">Sign Out</span>
-                    </a>
-                </li>
-            </ul>
-        </div>
+        <?php include('sidebar-2.php'); ?>
 
         <!-- ========================= Main ==================== -->
-        <div class="main">
-            <div class="topbar">
-                <div class="toggle">
-                    <ion-icon name="menu-outline"></ion-icon>
-                </div>
-
-                <div class="search">
-                    <label>
-                        <input type="text" placeholder="Search here">
-                        <ion-icon name="search-outline"></ion-icon>
-                        
-                    </label>
-                </div>
-
-                <div class="user">
-                    <img src="../images/default-profile.png" alt="">
-            
-                </div>
-            </div>
+            <?php include('header.php'); ?>
             <div class="content">
                 <div clas="nav-title"><h3>Dashboard</h3></div>
                 <!-- !!!!!!!!!!CODES HERE!!!!!!!! -->
