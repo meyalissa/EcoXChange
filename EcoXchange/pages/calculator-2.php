c22372aa43ead852c9ee4f4a3cbffb800cb0b432
Melissa Sofia <melissa.sofia0204@gmail.com>
2024-05-22 11:46:10 +0800

    change overflow

--- EcoXchange/pages/calculator-2.php
+++ EcoXchange/pages/calculator-2.php
@@ -100,6 +100,7 @@ $query = mysqli_query($dbconn, $sql) or die("Error: " . mysqli_error($dbconn));
     <!-- =========== Scripts =========  -->
     <script src="../js/main.js"></script>
     <script src="../js/calculatorfunc.js"></script>
+  
     <!-- ====== ionicons ======= -->
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
