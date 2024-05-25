058f9e20f9bb3aff436ba4dd444e1d3d80b1b463
Melissa Sofia <melissa.sofia0204@gmail.com>
2024-05-25 13:18:00 +0800

    Update members.php
    Bank details button working 100%
    
    members page completed

--- EcoXchange/pages/members.php
+++ EcoXchange/pages/members.php
@@ -27,7 +27,7 @@ include('../includes/fetchUserData.php');
         <div class="main">
             <?php include('header.php'); ?>
             <div class="content">
-                 <!-- !!!!!!!!!!CODES HERE!!!!!!!! -->
+                <!-- !!!!!!!!!!CODES HERE!!!!!!!! -->
                 <div class="details">
                     <div class="itemlist">
                         <div class="tableHeader">
@@ -38,101 +38,80 @@ include('../includes/fetchUserData.php');
                                 <ion-icon name="search-outline"></ion-icon>
                                 </label>
                             </div>
-                         
-                    </div>
+                        </div>
                         <?php
-                            
-                            $sql = "SELECT * FROM customer";
+                            // Query for Customer table
+                            $sql = "SELECT * FROM customer c JOIN bank_details b ON c.bank_id = b.bank_id";
                             $query = mysqli_query($dbconn, $sql);
                             $num_rows = mysqli_num_rows($query);
-                            if($num_rows == 0){
+                            if ($num_rows == 0) {
                                 echo "No item found";
                             } else {
                                 echo '<table class="table1">';
                                 echo "<thead>";
-                                echo"<tr>";
-                                echo"<td>Customer ID</td>";
-                                echo"<td>Username</td>";
-                                echo"<td>First Name</td>";
-                                echo"<td>Last Name</td>";
-                                echo"<td>Contact</td>";
-                                echo"<td>Email</td>";
-                                echo"<td>Picture</td>";
-                                echo"<td>Bank</td>";
-                                echo"</tr>";
+                                echo "<tr>";
+                                echo "<td>Customer ID</td>";
+                                echo "<td>Username</td>";
+                                echo "<td>First Name</td>";
+                                echo "<td>Last Name</td>";
+                                echo "<td>Contact</td>";
+                                echo "<td>Email</td>";
+                                echo "<td>Picture</td>";
+                                echo "<td>Bank</td>";
+                                echo "</tr>";
                                 echo "</thead>";
+                                echo "<tbody>";
 
-
-                                while($row = mysqli_fetch_array($query)){ 
-                                    echo "<tbody>";
-                                        echo"<tr>";
-                                            echo '<td class="idcust">' . $row["cust_ID"] . "</td>";
-                                            
-                                            echo"<td>".$row["cust_username"]."</td>";
-                                            echo"<td>".$row["cust_first_name"]."</td>";
-                                            echo"<td>".$row["cust_last_name"]."</td>";
-                                            echo"<td>".$row["cust_contact_no"]."</td>";
-                                            echo"<td>".$row["cust_email"]."</td>";
-                                            echo '<td><img src="' . $row['cust_pict'] . '" alt=""></td>';
-                                            echo '<td><button type="button" class="btnBank" id="btnBank">Show</button> </td>';
-                                            // echo"<td><a href='edit.php?item_ID=".$row["item_ID"]."'>Edit</a></td>";
-                                        echo"</tr>";
-                                    echo "</tbody>";
+                                while ($row = mysqli_fetch_array($query)) {
+                                    echo "<tr>";
+                                    echo '<td class="idcust">' . $row["cust_ID"] . "</td>";
+                                    echo "<td>" . $row["cust_username"] . "</td>";
+                                    echo "<td>" . $row["cust_first_name"] . "</td>";
+                                    echo "<td>" . $row["cust_last_name"] . "</td>";
+                                    echo "<td>" . $row["cust_contact_no"] . "</td>";
+                                    echo "<td>" . $row["cust_email"] . "</td>";
+                                    echo '<td><img src="' . $row['cust_pict'] . '" alt=""></td>';
+                                    echo '<td><button type="button" class="btnBank" id="btnBank" data-bank-name="' . $row["bank_name"] . '" data-bank-acc-no="' . $row["bank_acc_no"] . '" data-bank-full-name="' . $row["bank_full_name"] . '">Show</button></td>';
+                                    echo "</tr>";
                                 }
-                                echo '</table>';
-                            
-                            } 
+
+                                echo "</tbody>";
+                                echo "</table>";
+                            }
                         ?>
                     </div>
                 </div>
                 <!-- +++++++++++++++ BANK DETAILS +++++++++++++++ -->
-                <div class=bank-popup id="bank-popup">
+                <div class="bank-popup" id="bank-popup">
                     <div class="box-popup">
                         <div class="top-form">
                             <h2>Bank Details</h2>
                             <div class="close-popup" data-popup="#bank-popup">
                                 X
                             </div>
-                            
                         </div>
-                        <div class = "addrcb">
-                            
+                        <div class="addrcb">
                             <hr>
                             <table>
                                 <tr>
-                                    <td>
-                                        Bank Name: 
-                                    </td>
-                                    <td class="info">
-                                        Maybank
-                                    </td>
+                                    <td>Bank Name:</td>
+                                    <td class="info" id="bank-name"></td>
                                 </tr>
                                 <tr>
-                                    <td>
-                                        Account No:
-                                    </td>
-                                    <td  class="info">
-                                        0129102910291212
-                                    </td>
+                                    <td>Account No:</td>
+                                    <td class="info" id="bank-acc-no"></td>
                                 </tr>
                                 <tr>
-                                    <td>
-                                        Full Name :
-                                    </td>
-                                    <td  class="info">
-                                        Melissa Sofia Binti Shahran
-                                    </td>
+                                    <td>Full Name:</td>
+                                    <td class="info" id="bank-full-name"></td>
                                 </tr>
                             </table>
-
-
                         </div>
                     </div>  
                 </div>
             </div>
         </div>
     </div>
-
 
     <!-- =========== Scripts =========  -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> <!-- jQuery Library -->
@@ -148,6 +127,9 @@ include('../includes/fetchUserData.php');
 
             // Function to open bank details popups
             $('.btnBank').click(function() {
+                $('#bank-name').text($(this).data('bank-name'));
+                $('#bank-acc-no').text($(this).data('bank-acc-no'));
+                $('#bank-full-name').text($(this).data('bank-full-name'));
                 $('#bank-popup').fadeIn().css("display", "flex");
             });
         });
@@ -158,4 +140,3 @@ include('../includes/fetchUserData.php');
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
 </html>
-
