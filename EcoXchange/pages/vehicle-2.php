3608a2103c54f017afec38e8915f9a0072b6ccb0
Melissa Sofia <melissa.sofia0204@gmail.com>
2024-05-30 15:45:11 +0800

    Update vehicle-2.php

--- EcoXchange/pages/vehicle-2.php
+++ EcoXchange/pages/vehicle-2.php
@@ -28,7 +28,90 @@ include('../includes/fetchUserData.php');
             <?php include('header.php'); ?>
             <div class="content">
                 <!-- !!!!!!!!!!CODES HERE!!!!!!!! -->
-                
+                <div class="details">
+                    <div class="itemlist">
+                        <div class="tableHeader">
+                            <h2>Members</h2>
+                            <div class="searchbar">
+                                <label>
+                                <input type="text" name="" id="search-member" placeholder="member's name" onkeyup="searchmember()">
+                                <ion-icon name="search-outline"></ion-icon>
+                                </label>
+                            </div>
+                        </div>
+                        <?php
+                            // Query for Customer table
+                            $sql = "SELECT * FROM customer c JOIN bank_details b ON c.bank_id = b.bank_id";
+                            $query = mysqli_query($dbconn, $sql);
+                            $num_rows = mysqli_num_rows($query);
+                            if ($num_rows == 0) {
+                                echo "No item found";
+                            } else {
+                                echo '<table class="table1">';
+                                echo "<thead>";
+                                echo "<tr>";
+                                echo "<td>Customer ID</td>";
+                                echo "<td>Username</td>";
+                                echo "<td>First Name</td>";
+                                echo "<td>Last Name</td>";
+                                echo "<td>Contact</td>";
+                                echo "<td>Email</td>";
+                                echo "<td>Picture</td>";
+                                echo "<td>Bank</td>";
+                                echo "</tr>";
+                                echo "</thead>";
+                                echo "<tbody>";
+
+                                while ($row = mysqli_fetch_array($query)) {
+                                    echo "<tr>";
+                                    echo '<td class="idcust">' . $row["cust_ID"] . "</td>";
+                                    echo "<td>" . $row["cust_username"] . "</td>";
+                                    echo "<td>" . $row["cust_first_name"] . "</td>";
+                                    echo "<td>" . $row["cust_last_name"] . "</td>";
+                                    echo "<td>" . $row["cust_contact_no"] . "</td>";
+                                    echo "<td>" . $row["cust_email"] . "</td>";
+                                    echo '<td><img src="' . $row['cust_pict'] . '" alt=""></td>';
+                                    echo '<td><button type="button" class="btnBank" id="btnBank" data-bank-name="' . $row["bank_name"] . '" data-bank-acc-no="' . $row["bank_acc_no"] . '" data-bank-full-name="' . $row["bank_full_name"] . '">Show</button></td>';
+                                    echo "</tr>";
+                                }
+
+                                echo "</tbody>";
+                                echo "</table>";
+                            }
+                        ?>
+                    </div>
+                </div>
+                <!-- +++++++++++++++ BANK DETAILS +++++++++++++++ -->
+                <div class="bank-popup" id="bank-popup">
+                    <div class="box-popup">
+                        <div class="top-form">
+                            <h2>Bank Details</h2>
+                            <div class="close-popup" data-popup="#bank-popup">
+                                X
+                            </div>
+                        </div>
+                        <div class="bankdt">
+                            <hr>
+                            <table>
+                                <tr>
+                                    <td>Bank Name:</td>
+                                    <td class="info" id="bank-name"></td>
+                                </tr>
+                                <tr>
+                                    <td>Account No:</td>
+                                    <td class="info" id="bank-acc-no"></td>
+                                </tr>
+                                <tr>
+                                    <td>Full Name:</td>
+                                    <td class="info" id="bank-full-name"></td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>  
+                </div>
+            </div>
+        </div>
+    </div>
 
     <!-- =========== Scripts =========  -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> <!-- jQuery Library -->
